893f40497c3418001f4aee7fd23ac9271bb89b8e
anandaikhsan <anandamukhammadikhsan@gmail.com>
2020-01-31 03:58:02 +0700

    add error handling for socket hangup

--- routes/api/university/news.js
+++ routes/api/university/news.js
@@ -30,59 +30,87 @@ const cheerio = require('cheerio');
 // });
 
 router.get('/page/info', async function (req, res) {
-    const content = await rp('https://uny.ac.id/index-berita');
-    const $ = cheerio.load(content);
-
-    const firstIndex = 0;
-    const lastIndex = parseInt($('.pager-last.last a').attr('href').split('?')[1].split('=')[1]);
-    const totalPage = 1 + lastIndex;
-    const beritaPerPage = $('.view-berita .view-content table tr').length;
-    const result = {
-        firstIndex, lastIndex, totalPage, beritaPerPage
-    };
-
-    await res.json({result});
+    rp('https://uny.ac.id/index-berita', (err, resp, content) => {
+        try{
+            const $ = cheerio.load(content);
+            console.log(err);
+            
+            const firstIndex = 0;
+            const lastIndex = parseInt($('.pager-last.last a').attr('href').split('?')[1].split('=')[1]);
+            const totalPage = 1 + lastIndex;
+            const beritaPerPage = $('.view-berita .view-content table tr').length;
+            const result = {
+                firstIndex, lastIndex, totalPage, beritaPerPage
+            };
+            res.json({status: 200, result});
+        }catch(err){
+            console.log(err.message);
+            res.status(500).json({
+                status: 500,
+                error: err.message
+            })
+        }
+    });
 });
 
 router.get('/page/:pageNumber', async function (req, res) {
-    const content = await rp('https://uny.ac.id/index-berita?page='+req.params.pageNumber);
-    const $ = cheerio.load(content);
-    const result = {
-        results: []
-    };
-    $('.view-berita .view-content table tr').each((index, element) => {
-        const title = $(element).find('td .views-field-title .field-content a').text();
-        const summary = $(element).find('td .views-field-body .field-content p').text();
-        const link = $(element).find('td .views-field-title .field-content a').attr('href');
-        const content = null;
-        const image = $(element).find('td .views-field-image .field-content img').attr('src');
-        const created_date = $(element).find('td .views-field-created .field-content').text().split(': ')[1].split('\n')[0];
-
-        result.results.push({
-            title, summary, link, content, image, created_date
-        });
+    rp('https://uny.ac.id/index-berita?page='+req.params.pageNumber, (err, resp, content) => {
+        try{
+            const $ = cheerio.load(content);
+            const result = {
+                results: []
+            };
+            $('.view-berita .view-content table tr').each((index, element) => {
+                const title = $(element).find('td .views-field-title .field-content a').text();
+                const summary = $(element).find('td .views-field-body .field-content p').text();
+                const link = $(element).find('td .views-field-title .field-content a').attr('href');
+                const content = null;
+                const image = $(element).find('td .views-field-image .field-content img').attr('src');
+                const created_date = $(element).find('td .views-field-created .field-content').text().split(': ')[1].split('\n')[0];
+        
+                result.results.push({
+                    title, summary, link, content, image, created_date
+                });
+            });
+        
+            res.json({status: 200, result})
+        }catch(err){
+            console.log(err.message);
+            res.status(500).json({
+                status: 500,
+                error: err.message
+            })
+        }
     });
-
-    await res.json({result})
 });
 
 router.get('/open', async function(req, res){
-    const body = await rp('https://uny.ac.id/'+req.params.link);
-    const $ = cheerio.load(body);
+    rp('https://uny.ac.id/'+req.params.link, (err, resp, body) => {
+        try{
+            const $ = cheerio.load(body);
 
-    const title = $('h1.title a').text();
-    const summary = null;
-    const link = $('h1.title a').attr('href');
-    const content = $('.content.node-article .field-item.even').text();
-    const image = $('.field-name-field-image img').attr('src');
-    const submitted_date = $('#date-submitted');
-    const created_date = $(submitted_date).find('.day').text() + $(submitted_date).find('.month').text() + $(submitted_date).find('.year').text();
+            const title = $('h1.title a').text();
+            const summary = null;
+            const link = $('h1.title a').attr('href');
+            const content = $('.content.node-article .field-item.even').text();
+            const image = $('.field-name-field-image img').attr('src');
+            const submitted_date = $('#date-submitted');
+            const created_date = $(submitted_date).find('.day').text() + $(submitted_date).find('.month').text() + $(submitted_date).find('.year').text();
 
-    const result = {
-        title, summary, link, content, image, created_date
-    };
+            const result = {
+                title, summary, link, content, image, created_date
+            };
 
-    await res.json({result});
+            res.json({status: 200, result});
+        }catch(err){
+            console.log(err.message);
+            
+            res.status(500).json({
+                status: 500,
+                error: err.message
+            })
+        }
+    });
 })
 
 
--- utils/downloadContent.js
+++ utils/downloadContent.js
@@ -1,13 +1,25 @@
-const puppeteer = require('puppeteer');
+const request = require('request');
 
-module.exports = async function(url) {
-    const browser = await puppeteer.launch({
-        headless: true
-    })
+module.exports = function(url, cb) {
 
-    const page = await browser.newPage();
-    await page.setUserAgent("'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36'");
-    await page.goto(url);
+    var options = {
+        url,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36',
+          'Content-Length': 56071,
+          'Connection': 'keep-alive'
+        }
+      };
+      
+      
+      request(options, cb);
+    // const browser = await puppeteer.launch({
+    //     headless: true
+    // })
 
-    return await page.content();
+    // const page = await browser.newPage();
+    // await page.setUserAgent("'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36'");
+    // await page.goto(url);
+
+    // return await page.content();
 }
